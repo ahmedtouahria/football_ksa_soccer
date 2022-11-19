339fd7dda79dfb2b8a24ecba25352b14d236eadb
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2022-11-19 14:38:40 +0100

    initial otp logic

--- account/functions.py
+++ account/functions.py
@@ -7,15 +7,10 @@ def send_otp(phone):
     """
 
     if phone:
-
         key = otp_generator()
         phone = str(phone)
         otp_key = str(key)
         print("otp",otp_key)
-        link = f'https://2factor.in/API/R1/?module=TRANS_SMS&apikey=7c59cf94-d129-11ec-9c12-0200cd936042&to={phone}&from=MMBook&templatename=mymedbook&var1={otp_key}&var2={otp_key}'
-
-        result = requests.get(link, verify=False)
-
         return otp_key
     else:
         return False
