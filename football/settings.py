aff4a7c2a760b4feda17bad5150289b8bbb98cee
“ahmedtouahria” <ahmedtouahria2001@gmail.com>
2022-12-02 21:39:24 +0100

    handel gitignore

--- football/settings.py
+++ football/settings.py
@@ -158,13 +158,21 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'news_app/static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, '') # 'data' is my media folder
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
